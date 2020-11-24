beb8cb4b8af0b21d3bb457e5836bb7b623c10af1
Louis Leffler <louisleffler@gmail.com>
2020-11-24 23:16:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,9 +15,12 @@ def using_insert(array, element)
   new_array = captain_planet_and_the_planeteers.uniq
 end
 
-def using_flatten(private_colleges_in_newyork)
-  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-  flat_array = private_colleges_in_newyork.flatten
+def using_flatten
+  it "takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings" do 
+     instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+     flat_array = using_flatten(instruments)
+     expect(flat_array).to eq(["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"])
+  end
 end
 
 def using_delete(my_list, my_element)
