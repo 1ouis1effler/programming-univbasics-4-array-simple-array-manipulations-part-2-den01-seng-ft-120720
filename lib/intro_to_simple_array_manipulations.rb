7a4e298e7dbc5db91318e4a2358e6242ab03b33e
Louis Leffler <louisleffler@gmail.com>
2020-11-24 22:35:46 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,6 @@
+describe "using_concat" do 
+  before(:each) do 
+    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    @all_my_favs = using_concat(@my_favorite_things, more_favs)
+  end
