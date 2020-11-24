29de2e0da58ed237babf402564465dbd344938f1
Louis Leffler <louisleffler@gmail.com>
2020-11-24 23:14:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,5 +14,18 @@ def using_insert(array, element)
   captain_planet_and_the_planeteers =  ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler"]
   new_array = captain_planet_and_the_planeteers.uniq
 end
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+  flat_array = private_colleges_in_newyork.flatten
+end
 
+def using_delete(my_list, my_element)
+  my_list.delete(my_element)
+end
+
+def using_delete_at(famous_robots, integer)
+  famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+  integer = 2
+  deleted_robot = famous_robots.delete_at(integer)
+end
 
