7b777d934d8bfeaf6197c4aa09e3a39a57336eed
Louis Leffler <louisleffler@gmail.com>
2020-11-24 22:42:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,6 @@
-def using_concat(my_favorite_things, more_favs)
-  favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"]
-  favorite_things.concat(more_favs)
+def using_concat(cars, trucks)
+  cars = ["Ford", "Fiat", "Dodge"]
+  trucks = ["Ram", "F150", "Loader"]
+  
 end
+
