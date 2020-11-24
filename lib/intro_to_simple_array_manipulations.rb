873a0b1de1f251621d76cf91b27af9514f4b2d2b
Louis Leffler <louisleffler@gmail.com>
2020-11-24 22:45:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
-def using_concat(@all_my_favs, @)
-  cars = ["Ford", "Fiat", "Dodge"]
+def using_concat(@all_my_favs, @more_favs)
+  cars = ["]
   trucks = ["Ram", "F150", "Loader"]
   cars_and_trucks = cars.concat(trucks)
 end
